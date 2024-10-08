3fdc761288a6515b835232d56d5a4d8abe8153af
ChangJun Rho <geniusjun4663@gmail.com>
2024-10-08 18:50:32 +0900

    [Bronze V] Title: N 찍기, Time: 824 ms, Memory: 32984 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        int[] sum = new int[T];
+
+        for (int i = 1; i <= T; i++) {
+            System.out.println(i);
+        }
+    }
+}
+

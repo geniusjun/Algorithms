33cff3be06c0b159d39d47ccbd6d2bc3a3a5ca9a
ChangJun Rho <geniusjun4663@gmail.com>
2024-10-08 20:16:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        char strCtr = str.charAt(0);
+        System.out.println((int) strCtr);
+
+
+    }
+}
+

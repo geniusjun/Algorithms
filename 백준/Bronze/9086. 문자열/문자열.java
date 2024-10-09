370480ad79b66ad23a75188ce4baa2eb9745763c
ChangJun Rho <geniusjun4663@gmail.com>
2024-10-09 20:04:31 +0900

    [Bronze V] Title: 문자열, Time: 164 ms, Memory: 17388 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i < T; i++){
+            String text = sc.nextLine();
+            System.out.print(text.charAt(0));
+            System.out.print(text.charAt(text.length() - 1));
+            System.out.println();
+        }
+    }
+}
+

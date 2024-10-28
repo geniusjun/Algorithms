ca39c00893553f591f2f9abca9d41a599e2fad12
ChangJun Rho <geniusjun4663@gmail.com>
2024-10-29 02:15:02 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 304 ms, Memory: 20060 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args)  {
-
-        Scanner sc = new Scanner(System.in);
-
-
-        int N = sc.nextInt();
-        int count = 1;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j <N; j++) {
-                if(count% 2 ==1){
-                    System.out.print("*");
-                    System.out.print(" ");
-            } else{
-                    System.out.print(" ");
-                    System.out.print("*");
+            if (N == 1) {
+                System.out.println("*");
+            } else if (i % 2 == 0) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print("* ");
                 }
+                System.out.println();
+            } else {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(" *");
+                }
+                System.out.println();
             }
-            System.out.println();
-            count++;
         }
     }
 }

886648310065e651567a9ada6a867990bb97f99e
ChangJun Rho <geniusjun4663@gmail.com>
2024-11-04 20:49:45 +0900

    [Bronze III] Title: 택시 기하학, Time: 180 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double R = sc.nextDouble();
+
+        System.out.println(Math.PI * R*R);
+        System.out.println(2*R*R);
+    }
+}
+
+
+

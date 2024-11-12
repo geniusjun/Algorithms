0dff813c54c8b728b72a396d2eda919414746ddb
ChangJun Rho <geniusjun4663@gmail.com>
2024-11-13 01:28:03 +0900

    [Bronze II] Title: 블랙잭, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -8,20 +8,17 @@ public class Main {
 
         int N = sc.nextInt();
         int M = sc.nextInt();
-        int max = Integer.MIN_VALUE;
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
+        int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < N-2; i++) {
-            for (int j = i+1; j < N-1; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
                 for (int k = j+1; k < N; k++) {
-                    int temp = arr[i] + arr[j] + arr[k];
-
-                    if(temp>max && M>=temp){
-                        max = temp;
-                    }
+                int temp = arr[i] + arr[j] + arr[k];
+                if(temp>max && M >= temp) max = temp;
                 }
             }
         }

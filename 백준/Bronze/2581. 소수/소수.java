355c5f5d4a46edea50505be27f72c75d1b6059b0
ChangJun Rho <geniusjun4663@gmail.com>
2024-11-14 03:02:31 +0900

    [Bronze II] Title: 소수, Time: 168 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -11,26 +11,29 @@ public class Main {
         int minNum = Integer.MAX_VALUE;
         int sum = 0;
         boolean foundPrime = false;
+
         for (int i = M; i <= N ; i++) {
+            if (i < 2) continue; // 1은 소수가 아님
+
             boolean isPrimeNumber = true;
-            for (int j = 2; j < i; j++) {
-                if(i%j == 0 ){
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if(i % j == 0) {
                     isPrimeNumber = false;
                     break;
                 }
             }
-            if(isPrimeNumber && (i != 1)) {
+            if(isPrimeNumber) {
                 sum += i;
-                if(i<minNum) minNum = i;
+                if(i < minNum) minNum = i;
                 foundPrime = true;
             }
         }
+
         if(!foundPrime){
             System.out.println(-1);
-        }else{
+        } else {
             System.out.println(sum);
             System.out.println(minNum);
         }
-
-        }
     }
+}

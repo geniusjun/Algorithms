cbf7dc2056e0d5f32d2793883cbcbf54152c33db
ChangJun Rho <geniusjun4663@gmail.com>
2025-02-06 20:52:44 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string s;
+int main() {
+
+	cin >> s;
+
+	for (int i = 0; i < s.size() / 2; i++) {
+		if (s[i] != s[s.size() - 1 - i]) {
+			cout << 0 << '\n';
+			return 0;
+		}
+	}
+	cout << 1 << '\n';
+	return 0;
+}

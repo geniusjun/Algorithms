61ea862a05ae828a075fcb7cc64b0279e1e3427e
ChangJun Rho <geniusjun4663@gmail.com>
2025-02-11 10:39:28 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,40 +1,19 @@
 #include <bits/stdc++.h>
-
-using namespace std;
-
-int A, B, C;
-vector<pair<int, int>> v;
-int arr[101];
-int sum;
-int main() {
-	cin >> A >> B >> C;
-
-	for (int i = 0; i < 3; i++) {
-		int a, b;
-		cin >> a >> b;
-		v.push_back({a,b});
+using namespace std;   
+int A, B, C, a, b, cnt[104], ret;
+int main(){
+	cin >> A >> B >> C; 
+	for(int i = 0; i < 3; i++){
+		cin >> a >> b; 
+		for(int j = a; j < b; j++)cnt[j]++;
 	}
-	
-	for (int i = 0; i < 3; i++) {
-		for (int j = v[i].first; j < v[i].second; j++) {
-			arr[j]++;
+	for(int j = 1; j < 100; j++){
+		if(cnt[j]){
+			if(cnt[j] == 1) ret += A;
+			else if(cnt[j] == 2)ret += B * 2;
+			else if(cnt[j] == 3)ret += C * 3;
 		}
 	}
-
-	for (int i = 0; i < 101; i++) {
-		if (arr[i] == 1) {
-			sum += A;
-		}
-		else if (arr[i] == 2) {
-			sum += 2*B;
-		}
-		else if (arr[i] == 3) {
-			sum += 3*C;
-		}
-	}
-
-	cout << sum << endl;
-
-
-	return 0;
+	cout << ret << "\n"; 
+    return 0;
 }

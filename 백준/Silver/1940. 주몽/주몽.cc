8c87617311f578fbafd965d910e25376ed9fcc83
ChangJun Rho <geniusjun4663@gmail.com>
2025-02-17 20:19:16 +0900

    [Silver IV] Title: 주몽, Time: 80 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
-int N, cnt;
-ll M;
-vector<int> v;
+using namespace std;
+
+int N, M, ret;
+int a[15004], visited[15004];
 int main() {
 
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++) {
-		int temp;
-		cin >> temp;
-		v.push_back(temp);
+		cin >> a[i];
 	}
 
-	for (int i = 0; i < v.size() - 1; i++) {
-		for (int j = i+1; j < v.size(); j++) {
-			if (v[i] + v[j] == M) cnt++;
+	for (int i = 0; i < N; i++) {
+		for (int j = i+1; j < N; j++) {
+			if (a[i] + a[j] == M && visited[i] == 0 && visited[j] == 0) {
+				ret++;
+				visited[i] = 1, visited[j] = 1;
+			}
 		}
 	}
 
-	cout << cnt << '\n';
+	cout << ret << '\n';
+
 	return 0;
 }

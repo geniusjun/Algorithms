b7747b1ba3ad5e50685829ae076270b9300c15e2
ChangJun Rho <geniusjun4663@gmail.com>
2025-12-23 14:13:04 +0900

    [level 1] Title: 실패율, Time: 9.65 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,40 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int N, int[] stages) {
-        Map<Integer, Double> stageFail = new HashMap<>();
-        for(int i = 1; i <=N; i++){
-            int failCount = 0;
-            int reachedCount = 0;
-            for(int j = 0; j < stages.length; j++){
-                if (stages[j] == i) {
-                    failCount++;          // 실패자
-                }
-                if (stages[j] >= i) {
-                    reachedCount++;       // 도달자
-                }
+        public int[] solution(int N, int[] stages) {
+        int[] challenger = new int[N+2];
+        for(int i = 0; i < stages.length; i++){
+            challenger[stages[i]] += 1;
+        }
+        HashMap<Integer, Double> fails = new HashMap<>();
 
+        double total = stages.length;
+
+        for(int i = 1; i <= N; i++){
+            if(challenger[i] == 0){
+                fails.put(i, 0.);
+            }else{
+                fails.put(i, challenger[i] / total);
+                total -= challenger[i];
             }
-            double fail = (reachedCount == 0) ? 0.0 : (double) failCount / reachedCount;
-            stageFail.put(i, fail);
         }
-        List<Map.Entry<Integer, Double>> list =
-                new ArrayList<>(stageFail.entrySet());
-        list.sort((a, b) -> {
-            int cmp = Double.compare(b.getValue(), a.getValue());
-            if (cmp != 0) return cmp;
-            return Integer.compare(a.getKey(), b.getKey());
-        });
-
-
-        int[] answer = new int[N];
-        for(int i = 0; i < N; i++) {
-            answer[i] = list.get(i).getKey();
-        }
-        return answer;
+        return fails.entrySet().stream()
+                .sorted((o1, o2)
+                        -> Double.compare(o2.getValue(), o1.getValue()))
+                .mapToInt(Map.Entry::getKey).toArray();
     }
 }

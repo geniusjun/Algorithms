53373206971c212ef2643594da4f94d543d2ced1
ChangJun Rho <geniusjun4663@gmail.com>
2025-12-26 16:11:39 +0900

    [level 1] Title: 실패율, Time: 7.34 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,25 +2,24 @@ import java.util.*;
 
 class Solution {
         public int[] solution(int N, int[] stages) {
-        int[] challenger = new int[N+2];
+        int[] answer = new int[N+2];
+        HashMap<Integer, Double> map = new HashMap<>();
         for(int i = 0; i < stages.length; i++){
-            challenger[stages[i]] += 1;
+            answer[stages[i]]++;
         }
-        HashMap<Integer, Double> fails = new HashMap<>();
 
-        double total = stages.length;
-
+        double arrivers = stages.length;
         for(int i = 1; i <= N; i++){
-            if(challenger[i] == 0){
-                fails.put(i, 0.);
-            }else{
-                fails.put(i, challenger[i] / total);
-                total -= challenger[i];
+            if(answer[i] == 0){
+                map.put(i, 0.0);
+            }
+            else {
+                map.put(i, answer[i] / arrivers);
+                arrivers -= answer[i];
             }
         }
-        return fails.entrySet().stream()
-                .sorted((o1, o2)
-                        -> Double.compare(o2.getValue(), o1.getValue()))
-                .mapToInt(Map.Entry::getKey).toArray();
+
+        return map.entrySet().stream().sorted((o1,o2)
+                -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(Map.Entry::getKey).toArray();
     }
 }

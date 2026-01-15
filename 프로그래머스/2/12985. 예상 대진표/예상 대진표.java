007aa68d1f7b84223fd726bfd3fc610b207c46ac
ChangJun Rho <geniusjun4663@gmail.com>
2026-01-15 13:38:50 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,15 +1,22 @@
+import java.util.*;
+
 class Solution
 {
-    public int solution(int n, int a, int b)
-    {
-        int round = 0;
-
-        while(a != b){
-            a = (a+1) / 2;
-            b = (b+1) / 2;
-            round++;
+    public static int solution(int n, int a, int b) {
+        int cnt = 0;
+        while (Math.abs(b - a) >= 1) {
+            if (a % 2 == 0) {
+                a = a / 2;
+            } else {
+                a = a / 2 + 1;
+            }
+            if (b % 2 == 0) {
+                b = b / 2;
+            } else {
+                b = b / 2 + 1;
+            }
+            cnt++;
         }
-
-        return round;
+        return cnt;
     }
 }

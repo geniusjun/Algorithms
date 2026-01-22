b8c37fffe8e2a1776ba9b6b5898705be09950cd5
ChangJun Rho <geniusjun4663@gmail.com>
2026-01-22 23:22:00 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[26];
+        String s = bf.readLine();
 
-    public static void main(String[] args)  {
-
-        Scanner sc = new Scanner(System.in);
-        // 'a' = 97
-
-        char[] arr = sc.next().toCharArray(); //
-
-        int[] alphabet = new int[26];
-
-        for(int i = 0; i<arr.length; i++){
-            alphabet[(arr[i] - 97)]++;
-        }
-        for (int i : alphabet) {
-            System.out.print(i + " ");
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
         }
 
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
-}
 
+
+}

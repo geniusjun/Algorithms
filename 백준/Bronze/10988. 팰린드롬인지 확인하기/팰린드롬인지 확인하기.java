deaf34edf4a3c23d46e7684e252b425b2eddc426
ChangJun Rho <geniusjun4663@gmail.com>
2026-01-25 14:19:38 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,30 +1,25 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String word = bf.readLine();
 
-        String input = sc.nextLine();
+        boolean isPal = true;
 
-        char[] arr = input.toCharArray();
-        char[] arr2 = new char[arr.length];
-        int count = 0;
-        for (int i = arr.length-1; i >=0 ; i--) {
-            arr2[count] = arr[i];
-            count++;
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) - '0' != word.charAt(word.length() - 1 - i) - '0') {
+                isPal = false;
+            }
         }
 
-
-        if(Arrays.equals(arr, arr2)){
-            System.out.println(1);
-        }else {
-            System.out.println(0);
-        }
-
+        if (isPal) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
         }
     }
-
-
-
+}

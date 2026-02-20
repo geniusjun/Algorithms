597cfdb1ec730c8c9e8996d205832363a3dede56
ChangJun Rho <geniusjun4663@gmail.com>
2026-02-20 11:56:59 +0900

    [Platinum V] Title: 백조의 호수, Time: 736 ms, Memory: 162776 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -6,16 +6,18 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int Y, X;
-    static char[][] maps;
-    static boolean[][] visited;
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int day;
-    static ArrayDeque<Node> waterQ;
+
+    static int Y;
+    static int X;
+    static char[][] maps;
+    static int[][] visited;
+    static Node swan;
     static ArrayDeque<Node> swanQ;
     static ArrayDeque<Node> nextSwanQ;
-    static Node swan1, swan2;
+    static ArrayDeque<Node> waterQ;
+    static int day;
 
     static class Node {
         int y;
@@ -34,55 +36,56 @@ public class Main {
                 int ny = front.y + dy[i];
                 int nx = front.x + dx[i];
 
-                if (ny == swan2.y && nx == swan2.x) {
-                    return true;
-                }
-
-                if (ny < 0 || nx < 0 || ny >= Y || nx >= X || visited[ny][nx]) {
+                if (nx < 0 || ny < 0 || ny >= Y || nx >= X || visited[ny][nx] != 0) {
                     continue;
                 }
-                visited[ny][nx] = true;
-
-                if (maps[ny][nx] == 'X') {
-                    nextSwanQ.add(new Node(ny, nx));
-                }
                 if (maps[ny][nx] == '.') {
+                    visited[ny][nx] = 1;
                     swanQ.add(new Node(ny, nx));
+                } else if (maps[ny][nx] == 'X') {
+                    visited[ny][nx] = 1;
+                    nextSwanQ.add(new Node(ny, nx));
+                } else if (maps[ny][nx] == 'L') {
+                    return true;
                 }
             }
+
         }
         return false;
     }
 
     static ArrayDeque<Node> melt() {
-        ArrayDeque<Node> nextWaterQ = new ArrayDeque<>();
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+
         while (!waterQ.isEmpty()) {
             Node front = waterQ.poll();
             for (int i = 0; i < 4; i++) {
                 int ny = front.y + dy[i];
                 int nx = front.x + dx[i];
+
                 if (ny < 0 || nx < 0 || ny >= Y || nx >= X) {
                     continue;
                 }
+
                 if (maps[ny][nx] == 'X') {
                     maps[ny][nx] = '.';
-                    nextWaterQ.add(new Node(ny, nx));
+                    queue.add(new Node(ny, nx));
                 }
             }
         }
-        return nextWaterQ;
+        return queue;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         Y = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-        waterQ = new ArrayDeque<>();
-        swanQ = new ArrayDeque<>();
-        nextSwanQ = new ArrayDeque<>();
+
         maps = new char[Y][X];
-        visited = new boolean[Y][X];
+        visited = new int[Y][X];
+        waterQ = new ArrayDeque<>();
 
         for (int i = 0; i < Y; i++) {
             String line = bf.readLine();
@@ -92,19 +95,17 @@ public class Main {
                     waterQ.add(new Node(i, j));
                 }
                 if (input == 'L') {
-                    if (swan1 == null) {
-                        swan1 = new Node(i, j);
-                    } else {
-                        swan2 = new Node(i, j);
-                    }
+                    swan = new Node(i, j);
                 }
                 maps[i][j] = input;
             }
         }
 
         day = 0;
-        visited[swan1.y][swan1.x] = true;
-        swanQ.add(swan1);
+        swanQ = new ArrayDeque<>();
+        nextSwanQ = new ArrayDeque<>();
+        swanQ.add(swan);
+        visited[swan.y][swan.x] = 1;
         while (true) {
             if (swanMeet()) {
                 System.out.println(day);
@@ -116,6 +117,7 @@ public class Main {
             swanQ = nextSwanQ;
             nextSwanQ = new ArrayDeque<>();
             day++;
+
         }
     }
 }

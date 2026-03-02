09227e15d1840dcda242fa16d57f1defa6210884
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-02 14:18:12 +0900

    [Gold IV] Title: 알파벳, Time: 2036 ms, Memory: 300548 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,60 +6,51 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int Y, X, ret;
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int Y;
-    static int X;
     static char[][] maps;
-    static int ret;
+    static Set<Character> visitMap;
 
-    static class Node {
-        int y;
-        int x;
-
-        Node(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-
-    }
-
-    static void go(Set<Character> set, int idx, Node here) {
-        ret = Math.max(idx, ret);
-
+    static void go(int idx, int y, int x) {
+        ret = Integer.max(ret, idx);
         for (int i = 0; i < 4; i++) {
-            int ny = here.y + dy[i];
-            int nx = here.x + dx[i];
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
             if (ny < 0 || nx < 0 || ny >= Y || nx >= X) {
                 continue;
             }
-            if (!set.contains(maps[ny][nx])) {
-                set.add(maps[ny][nx]);
-                go(set, idx + 1, new Node(ny, nx));
-                set.remove(maps[ny][nx]);
+            if (visitMap.contains(maps[ny][nx])) {
+                continue;
             }
+            visitMap.add(maps[ny][nx]);
+            go(idx + 1, ny, nx);
+            visitMap.remove(maps[ny][nx]);
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         Y = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
 
         maps = new char[Y][X];
-        Set<Character> visitSet = new HashSet<>();
+        visitMap = new HashSet<>();
 
         for (int i = 0; i < Y; i++) {
-            String line = bf.readLine();
+            String input = bf.readLine();
             for (int j = 0; j < X; j++) {
-                maps[i][j] = line.charAt(j);
+                maps[i][j] = input.charAt(j);
             }
         }
 
-        visitSet.add(maps[0][0]);
-        go(visitSet, 1, new Node(0, 0));
+        ret = Integer.MIN_VALUE;
+        visitMap.add(maps[0][0]);
+        go(1, 0, 0);
+
         System.out.println(ret);
+
     }
 }

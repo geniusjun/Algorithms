801babc331529a882511d49c2480d1e08c89429f
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-08 12:30:57 +0900

    [Gold IV] Title: 가르침, Time: 2592 ms, Memory: 309460 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,52 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static int[] words;
-    static String s;
+    static int N, K, ret;
+    static Set<Character> set;
+    static List<String> words;
 
-    static int count(int mask) {
+    static void go(Set<Character> set, int idx) {
+        if (set.size() == K) {
+            if (!set.contains('a') || !set.contains('n') || !set.contains('t') || !set.contains('i') || !set.contains(
+                    'c')) {
+                return;
+            }
+
+            ret = Math.max(ret, readWord(set));
+            return;
+        }
+
+        for (int i = idx; i < 26; i++) {
+            set.add((char) (i + 97));
+            go(set, i + 1);
+            set.remove((char) (i + 97));
+        }
+    }
+
+    static int readWord(Set<Character> set) {
         int cnt = 0;
-        for (int i = 0; i < words.length; i++) {
-            int word = words[i];
-            if (word != 0 && (word & mask) == word) {
+
+        for (int i = 0; i < words.size(); i++) {
+            boolean isContain = true;
+            for (int j = 0; j < words.get(i).length(); j++) {
+                if (!set.contains(words.get(i).charAt(j))) {
+                    isContain = false;
+                    break;
+                }
+            }
+            if (isContain) {
                 cnt++;
             }
         }
         return cnt;
     }
 
-    static int go(int idx, int k, int mask) {
-        if (k < 0) {
-            return 0;
-        }
-        if (idx == 26) {
-            return count(mask);
-        }
-        int ret = go(idx + 1, k - 1, mask | (1 << idx));
-        if (idx != 'a' - 'a' && idx != 'n' - 'a' && idx != 't' - 'a' && idx != 'i' - 'a' && idx != 'c' - 'a') {
-            ret = Math.max(ret, go(idx + 1, k, mask));
-        }
-
-        return ret;
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        words = new int[51];
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken()); // 0~25
+
+        words = new ArrayList<>();
+        set = new HashSet<>();
+
         for (int i = 0; i < N; i++) {
-            s = bf.readLine();
-            for (int j = 0; j < s.length(); j++) {
-                words[i] |= (1 << s.charAt(j) - 'a');
-            }
+            words.add(bf.readLine());
         }
-        System.out.println(go(0, M, 0));
+
+        ret = 0;
+
+        go(set, 0);
+
+        System.out.println(ret);
     }
 }

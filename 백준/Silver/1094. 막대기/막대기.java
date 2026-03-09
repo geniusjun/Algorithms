d03244efc70163c158ade91ffc47d531fa4569d2
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-09 13:03:21 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,43 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
-    static int X, ret;
-    static List<Integer> list;
-
-    static void go(int sum, List<Integer> list) {
-        if (sum < X) {
-            return;
-        }
-        if (sum == X) {
-            ret = list.size();
-            return;
-        }
-
-        int shortestHalf = list.get(list.size() - 1) / 2;
-        if (sum - shortestHalf >= X) { // 버리고
-            list.remove(list.size() - 1);
-            list.add(shortestHalf);
-            go(sum - shortestHalf, list);
-        } else { // 안버리고
-            list.remove(list.size() - 1);
-            list.add(shortestHalf);
-            list.add(shortestHalf);
-            go(sum, list);
-        }
-        return;
-    }
+    static int N, ret;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        X = Integer.parseInt(bf.readLine());
-        list = new ArrayList<>();
-        list.add(64);
-        go(64, list);
+        N = Integer.parseInt(bf.readLine());
+        ret = 1;
+
+        while (N != 1) {
+            if ((N & 1) != 0) {
+                ret++;
+            }
+            N /= 2;
+        }
 
         System.out.println(ret);
     }

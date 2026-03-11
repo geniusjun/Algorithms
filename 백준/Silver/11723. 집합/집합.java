e31112325ede888697c6a8e3ea55e76b34d69517
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-11 13:43:33 +0900

    [Silver V] Title: 집합, Time: 1040 ms, Memory: 312908 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,79 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int M;
-    static Set<Integer> set;
+    static int N, M;
     static StringBuilder sb;
-
-    static void add(int x) {
-        set.add(x);
-    }
-
-    static void remove(int x) {
-        set.remove(x);
-    }
-
-    static void check(int x) {
-        if (set.contains(x)) {
-            sb.append("1").append("\n");
-        } else {
-            sb.append("0").append("\n");
-        }
-    }
-
-    static void toggle(int x) {
-        if (set.contains(x)) {
-            set.remove(x);
-        } else {
-            set.add(x);
-        }
-    }
-
-    static void all() {
-        for (int i = 1; i <= 20; i++) {
-            set.add(i);
-        }
-    }
-
-    static void empty() {
-        set.clear();
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = 0;
         M = Integer.parseInt(bf.readLine());
-        set = new HashSet<>();
         sb = new StringBuilder();
 
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             String oper = st.nextToken();
             if (oper.equals("all")) {
-                all();
+                N = (1 << 21) - 1;
                 continue;
             }
             if (oper.equals("empty")) {
-                empty();
+                N = 0;
                 continue;
             }
             int x = Integer.parseInt(st.nextToken());
             if (oper.equals("add")) {
-                add(x);
+                N |= (1 << x);
             } else if (oper.equals("remove")) {
-                remove(x);
+                N &= ~(1 << x);
             } else if (oper.equals("check")) {
-                check(x);
+                if (((N & (1 << x)) != 0)) {
+                    sb.append("1").append("\n");
+                } else {
+                    sb.append("0").append("\n");
+                }
             } else if (oper.equals("toggle")) {
-                toggle(x);
+                N ^= (1 << x);
             }
         }
-
         System.out.println(sb);
     }
 }

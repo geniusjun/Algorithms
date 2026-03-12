af94c327bba84c386a37adfe79adb0484b5d1d71
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-12 15:59:25 +0900

    [Gold III] Title: 종이 조각, Time: 176 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.java
+++ 백준/Gold/14391. 종이 조각/종이 조각.java
@@ -1,86 +1,64 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static int[][] map;
-    static int[][] visited;
-    static int answer = 0;
+    static int Y, X, ret;
+    static int[][] maps;
 
-    static void dfs(int idx) {
-        if (idx == N * M) {
-            calc();
-            return;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        Y = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        maps = new int[Y][X];
+
+        for (int i = 0; i < Y; i++) {
+            String input = bf.readLine();
+            for (int j = 0; j < X; j++) {
+                maps[i][j] = input.charAt(j) - '0';
+            }
         }
+        ret = 0;
 
-        int y = idx / M;
-        int x = idx % M;
-
-        // 가로
-        visited[y][x] = 1;
-        dfs(idx + 1);
-
-        // 세로
-        visited[y][x] = 0;
-        dfs(idx + 1);
-    }
-
-    static void calc() {
-        int sum = 0;
-
-        // 가로 계산
-        for (int i = 0; i < N; i++) {
-            int num = 0;
-            for (int j = 0; j < M; j++) {
-                if (visited[i][j] == 1) {
-                    num = num * 10 + map[i][j];
-                } else {
-                    sum += num;
-                    num = 0;
+        for (int s = 0; s < (1 << Y * X); s++) {
+            int sum = 0;
+            // 가로 계산
+            for (int i = 0; i < Y; i++) {
+                int cur = 0;
+                for (int j = 0; j < X; j++) {
+                    int k = i * X + j;
+                    if ((s & (1 << k)) == 0) {
+                        cur = cur * 10 + maps[i][j];
+                    } else {
+                        sum += cur;
+                        cur = 0;
+                    }
                 }
+                sum += cur;
             }
-            sum += num;
-        }
 
-        // 세로 계산
-        for (int j = 0; j < M; j++) {
-            int num = 0;
-            for (int i = 0; i < N; i++) {
-                if (visited[i][j] == 0) {
-                    num = num * 10 + map[i][j];
-                } else {
-                    sum += num;
-                    num = 0;
+            // 새로 계산
+            for (int j = 0; j < X; j++) {
+                int cur = 0;
+                for (int i = 0; i < Y; i++) {
+                    int k = i * X + j;
+                    if ((s & (1 << k)) != 0) {
+                        cur = cur * 10 + maps[i][j];
+                    } else {
+                        sum += cur;
+                        cur = 0;
+                    }
                 }
+                sum += cur;
             }
-            sum += num;
+            ret = Math.max(sum, ret);
         }
 
-        answer = Math.max(answer, sum);
-    }
+        System.out.println(ret);
 
-    public static void main(String[] args) throws Exception {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        map = new int[N][M];
-        visited = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = s.charAt(j) - '0';
-            }
-        }
-
-        dfs(0);
-
-        System.out.println(answer);
     }
 }

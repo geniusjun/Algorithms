6a0ac7246c0c48b06e134cfdf7640abcdba10c70
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-12 16:08:51 +0900

    [Gold III] Title: 종이 조각, Time: 148 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.java
+++ 백준/Gold/14391. 종이 조각/종이 조각.java
@@ -7,6 +7,57 @@ public class Main {
 
     static int Y, X, ret;
     static int[][] maps;
+    static int[][] type; // 가로 0, 세로 1
+
+    static void dfs(int idx) {
+        if (idx == Y * X) {
+            calc();
+            return;
+        }
+
+        int y = idx / X;
+        int x = idx % X;  // 이 아이디어 좋다,,,
+
+        type[y][x] = 0; // 가로 선택
+        dfs(idx + 1);
+
+        type[y][x] = 1; // 세로 선택
+        dfs(idx + 1);
+    }
+
+    static void calc() {
+        int sum = 0;
+
+        // 가로 세기
+        for (int i = 0; i < Y; i++) {
+            int cur = 0;
+            for (int j = 0; j < X; j++) {
+                if (type[i][j] != 0) {
+                    cur = cur * 10 + maps[i][j];
+                } else {
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
+        }
+        // 새로 세기
+        for (int j = 0; j < X; j++) {
+            int cur = 0;
+            for (int i = 0; i < Y; i++) {
+                if (type[i][j] != 1) {
+                    cur = cur * 10 + maps[i][j];
+                } else {
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
+        }
+
+        ret = Math.max(ret, sum);
+    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -15,6 +66,7 @@ public class Main {
         Y = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
         maps = new int[Y][X];
+        type = new int[Y][X];
 
         for (int i = 0; i < Y; i++) {
             String input = bf.readLine();
@@ -22,42 +74,9 @@ public class Main {
                 maps[i][j] = input.charAt(j) - '0';
             }
         }
-        ret = 0;
 
-        for (int s = 0; s < (1 << Y * X); s++) {
-            int sum = 0;
-            // 가로 계산
-            for (int i = 0; i < Y; i++) {
-                int cur = 0;
-                for (int j = 0; j < X; j++) {
-                    int k = i * X + j;
-                    if ((s & (1 << k)) == 0) {
-                        cur = cur * 10 + maps[i][j];
-                    } else {
-                        sum += cur;
-                        cur = 0;
-                    }
-                }
-                sum += cur;
-            }
-
-            // 새로 계산
-            for (int j = 0; j < X; j++) {
-                int cur = 0;
-                for (int i = 0; i < Y; i++) {
-                    int k = i * X + j;
-                    if ((s & (1 << k)) != 0) {
-                        cur = cur * 10 + maps[i][j];
-                    } else {
-                        sum += cur;
-                        cur = 0;
-                    }
-                }
-                sum += cur;
-            }
-            ret = Math.max(sum, ret);
-        }
-
+        ret = Integer.MIN_VALUE;
+        dfs(0);
         System.out.println(ret);
 
     }

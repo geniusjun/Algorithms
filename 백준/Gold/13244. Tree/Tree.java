ededdee99fa1f5c62d6eee96b2c2aed2217e22c7
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-13 15:39:26 +0900

    [Gold IV] Title: Tree, Time: 156 ms, Memory: 18172 KB -BaekjoonHub

--- 백준/Gold/13244. Tree/Tree.java
+++ 백준/Gold/13244. Tree/Tree.java
@@ -2,30 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int T, N, M;
     static List<Integer>[] adjList;
-    static boolean isGraph;
-    static Set<Integer> set;
+    static int[] visited;
 
     static int dfs(int idx) {
-        if (set.contains(idx)) {
-            return 0;
-        }
         int cnt = 1;
-        set.add(idx);
-
+        visited[idx] = 1;
         for (int i = 0; i < adjList[idx].size(); i++) {
             int next = adjList[idx].get(i);
-            cnt += dfs(next);
+            if (visited[next] == 0) {
+                cnt += dfs(next);
+            }
         }
-
         return cnt;
     }
 
@@ -34,10 +28,10 @@ public class Main {
         T = Integer.parseInt(bf.readLine());
 
         while (T-- > 0) {
-            isGraph = false;
             N = Integer.parseInt(bf.readLine());
             M = Integer.parseInt(bf.readLine());
             adjList = new ArrayList[N + 1];
+            visited = new int[N + 1];
             for (int i = 0; i < N + 1; i++) {
                 adjList[i] = new ArrayList<>();
             }
@@ -45,32 +39,12 @@ public class Main {
                 StringTokenizer st = new StringTokenizer(bf.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                for (int j = 0; j < adjList[a].size(); j++) {
-                    if (adjList[a].get(j) == b) {
-                        isGraph = true;
-                        break;
-                    }
-                }
-                for (int j = 0; j < adjList[b].size(); j++) {
-                    if (adjList[b].get(j) == a) {
-                        isGraph = true;
-                        break;
-                    }
-                }
 
                 adjList[a].add(b);
                 adjList[b].add(a);
             }
 
-            set = new HashSet<>();
-            if (dfs(1) != N) {
-                isGraph = true;
-            }
-
-            if (M != N - 1) {
-                isGraph = true;
-            }
-            if (isGraph) {
+            if (M != N - 1 || dfs(1) != N) {
                 System.out.println("graph");
             } else {
                 System.out.println("tree");

a4656ca47f2c7ee6db1edc32b0f232478e604a42
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-16 13:51:39 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 204 ms, Memory: 23672 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.java
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.java
@@ -7,30 +7,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        int ret = Integer.MIN_VALUE;
-        int[] arr = new int[n + 4];
+        int ret = 0;
         Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
         String input = bf.readLine();
 
         for (int i = 0; i < n; i++) {
             if (input.charAt(i) == '(') {
                 stack.push(i);
-            } else if (!stack.isEmpty()) { // '(' 가 들어왔고, ')'가 들어오는 경우
-                arr[stack.pop()] = 1;
-                arr[i] = 1;
+            } else { // ')'
+                stack.pop();
+                if (!stack.isEmpty()) {
+                    ret = Math.max(ret, i - stack.peek());
+                } else {
+                    stack.push(i);
+                }
             }
         }
-
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 1) {
-                cnt++;
-            } else {
-                ret = Math.max(ret, cnt);
-                cnt = 0;
-            }
-        }
-        ret = Math.max(ret, cnt);
 
         System.out.println(ret);
     }

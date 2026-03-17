2eab8dde0e8e2e3599a0675b68a0d5fbe2534532
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-17 16:39:19 +0900

    [Gold III] Title: 순회강연, Time: 240 ms, Memory: 20632 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Lecture {
+        int price;
+        int day;
+
+        Lecture(int price, int day) {
+            this.price = price;
+            this.day = day;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Lecture> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int price = Integer.parseInt(st.nextToken());
+            int day = Integer.parseInt(st.nextToken());
+            list.add(new Lecture(price, day));
+        }
+
+        list.sort((o1, o2) -> {
+            return o1.day - o2.day;
+        });
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            pq.add(list.get(i).price);
+            while (pq.size() > list.get(i).day) {
+                pq.poll();
+            }
+        }
+
+        int ret = 0;
+        while (!pq.isEmpty()) {
+            ret += pq.poll();
+        }
+
+        System.out.println(ret);
+    }
+}

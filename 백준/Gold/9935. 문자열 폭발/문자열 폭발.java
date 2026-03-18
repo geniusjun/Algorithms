41cd802b1b80b946a3630bac0056644a82bb9850
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-18 13:49:14 +0900

    [Gold IV] Title: 문자열 폭발, Time: 576 ms, Memory: 157248 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -14,35 +14,9 @@ public class Main {
 
         for (int i = 0; i < input.length(); i++) {
             char word = input.charAt(i);
-            if (!deque.isEmpty()) {
-                if (bomb.indexOf(word) == -1) { // 폭발 문자열 아닌경우
-                    deque.addLast(word);
-                } else { // 폭발 문자열인 경우
-                    deque.addLast(word); // 넣고 tempQ에 옮겨서 확인해보기
-                    boolean isBomb = false;
-                    ArrayDeque<Character> tempQ = new ArrayDeque<>();
-                    for (int j = bomb.length() - 1; j >= 0; j--) {
-                        if (deque.isEmpty()) {
-                            isBomb = false;
-                            break;
-                        }
-                        if (deque.peekLast() == bomb.charAt(j)) {
-                            tempQ.addLast(deque.pollLast());
-                            isBomb = true;
-                        } else {
-                            isBomb = false;
-                            break;
-                        }
-                    }
-                    if (!isBomb) { // 만약 tempQ에 있는게 폭발 문자열이 아니라면 원복
-                        while (!tempQ.isEmpty()) {
-                            deque.addLast(tempQ.pollLast());
-                        }
-                    }
-                }
-            } else {
-                // 비었는데 처음 들어오는 게 폭발 문자열인 경우
-                deque.addLast(word); // 일단 넣고 tempQ에 옮겨서 확인해보기
+            deque.add(word);
+
+            if (bomb.indexOf(word) != -1) { // 폭발 문자열인 경우
                 boolean isBomb = false;
                 ArrayDeque<Character> tempQ = new ArrayDeque<>();
                 for (int j = bomb.length() - 1; j >= 0; j--) {

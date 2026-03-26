a58d32c2ec055822c90ce17a4a743466c9901c4e
ChangJun Rho <geniusjun4663@gmail.com>
2026-03-26 15:04:53 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 336 ms, Memory: 28940 KB -BaekjoonHub

---
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -0,0 +1,138 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int Y, X, T, ret;
+    static int[][] maps, visited;
+    static int cleanerY;
+
+    static void dustGo() {
+        // 동시에 퍼져야 하기에 각각 퍼지는 값을 visited 배열에 축적하고 원본배열은 마이너스
+        for (int i = 0; i < Y; i++) {
+            for (int j = 0; j < X; j++) {
+                if (maps[i][j] != 0) {
+                    int dDust = maps[i][j] / 5;
+                    for (int k = 0; k < 4; k++) {
+                        int ny = i + dy[k];
+                        int nx = j + dx[k];
+                        if (!canGo(ny, nx)) {
+                            continue;
+                        }
+                        visited[ny][nx] += dDust;
+                        maps[i][j] -= dDust;
+                    }
+                }
+            }
+        }
+        // 원본배열에 퍼진 값 더해주기
+        for (int i = 0; i < Y; i++) {
+            for (int j = 0; j < X; j++) {
+                maps[i][j] += visited[i][j];
+            }
+        }
+    }
+
+    static void cleanUp() {
+        // 왼쪽 ↓
+        for (int i = cleanerY - 1; i >= 1; i--) {
+            maps[i][0] = maps[i - 1][0];
+        }
+
+        // 위쪽 ←
+        for (int i = 0; i < X - 1; i++) {
+            maps[0][i] = maps[0][i + 1];
+        }
+
+        // 오른쪽 ↑
+        for (int i = 0; i < cleanerY; i++) {
+            maps[i][X - 1] = maps[i + 1][X - 1];
+        }
+
+        // 아래쪽 →
+        for (int i = X - 1; i > 1; i--) {
+            maps[cleanerY][i] = maps[cleanerY][i - 1];
+        }
+
+        // 공기청정기 옆
+        maps[cleanerY][1] = 0;
+    }
+
+    static void cleanDown() {
+        int cleanerDownY = cleanerY + 1;
+
+        // 왼쪽 ↑
+        for (int i = cleanerDownY + 1; i < Y - 1; i++) {
+            maps[i][0] = maps[i + 1][0];
+        }
+
+        // 아래쪽 ←
+        for (int i = 0; i < X - 1; i++) {
+            maps[Y - 1][i] = maps[Y - 1][i + 1];
+        }
+
+        // 오른쪽 ↓
+        for (int i = Y - 1; i > cleanerDownY; i--) {
+            maps[i][X - 1] = maps[i - 1][X - 1];
+        }
+
+        // 위쪽 →
+        for (int i = X - 1; i > 1; i--) {
+            maps[cleanerDownY][i] = maps[cleanerDownY][i - 1];
+        }
+
+        // 공기청정기 옆
+        maps[cleanerDownY][1] = 0;
+    }
+
+    static boolean canGo(int y, int x) {
+        if (y < 0 || x < 0 || y >= Y || x >= X || maps[y][x] == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Y = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        maps = new int[Y][X];
+        cleanerY = 0;
+
+        for (int i = 0; i < Y; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < X; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num == -1 && cleanerY == 0) { // 공기청정기는 위의 y값만 저장
+                    cleanerY = i;
+                }
+                maps[i][j] = num;
+            }
+        }
+
+        while (T-- > 0) {
+            visited = new int[Y][X];
+            dustGo();
+            cleanUp();
+            cleanDown();
+        }
+
+        for (int i = 0; i < Y; i++) {
+            for (int j = 0; j < X; j++) {
+                if (maps[i][j] == 0 || maps[i][j] == -1) {
+                    continue;
+                }
+                ret += maps[i][j];
+            }
+        }
+
+        System.out.println(ret);
+    }
+}

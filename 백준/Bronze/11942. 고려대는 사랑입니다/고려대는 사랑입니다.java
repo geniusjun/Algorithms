8d22cb2e781f4c427c7358db9e3674770d586753
ChangJun Rho <geniusjun4663@gmail.com>
2026-04-13 19:13:24 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 92 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+       
+        System.out.println("고려대학교");
+    }
+}

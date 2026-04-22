79d8d24a6fce0c233cd0a56d08d088edbe61bcea
ChangJun Rho <geniusjun4663@gmail.com>
2026-04-22 13:46:08 +0900

    Title: 반복 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260422/반복 출력하기/repeated-output.java
+++ 260422/반복 출력하기/repeated-output.java
@@ -1,12 +1,13 @@
-import java.*;
+import java.io.*;
+import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         for(int i = 0; i < n; i++){
-            System.out.println("12345^&*()_")
+            System.out.println("12345^&*()_");
         }
     }
 }

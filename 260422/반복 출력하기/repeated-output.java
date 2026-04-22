11080d605ca02e638cc865add5172947c22a63aa
ChangJun Rho <geniusjun4663@gmail.com>
2026-04-22 13:40:19 +0900

    Title: 반복 출력하기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260422/반복 출력하기/repeated-output.java
@@ -0,0 +1,12 @@
+import java.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < n; i++){
+            System.out.println("12345^&*()_")
+        }
+    }
+}

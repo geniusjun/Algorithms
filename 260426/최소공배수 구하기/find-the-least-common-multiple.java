2ee5ce30281709317e4036b1bfbd35ac6287787d
ChangJun Rho <geniusjun4663@gmail.com>
2026-04-26 13:58:39 +0900

    Title: 최소공배수 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260426/최소공배수 구하기/find-the-least-common-multiple.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int a = n;
+        int b = m;
+
+        while(m != 0){
+            int temp = n % m;
+            n = m;
+            m = temp;
+        }
+
+        System.out.println((a * b) / n);
+        
+    }
+}

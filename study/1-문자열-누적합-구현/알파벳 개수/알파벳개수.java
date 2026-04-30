99691ad2dd6ae6c81468399d78ca8a290b51e50e
geniusjun <geniusjun4663@gmail.com>
2026-04-30 14:52:51 +0900

    1-알파벳 개수 문제 풀이

--- study/1-문자열-누적합-구현/알파벳 개수/알파벳개수.java
+++ study/1-문자열-누적합-구현/알파벳 개수/알파벳개수.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class 알파벳개수 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

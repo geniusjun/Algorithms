3d5433c025fbe08f901adaefcf8de6dcc1643a7a
geniusjun <geniusjun4663@gmail.com>
2026-04-30 15:09:41 +0900

    1-트럭주차 풀이

--- study/1-문자열-누적합-구현/트럭주차/트럭주차.java
+++ study/1-문자열-누적합-구현/트럭주차/트럭주차.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class 트럭주차 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

48166000b20d5c05504e61f92ddb1b9a863c229d
geniusjun <geniusjun4663@gmail.com>
2026-05-04 17:07:30 +0900

    1-한국이 그리울 땐 서버에 접속하지 풀이 추가

--- study/1-문자열-누적합-구현/한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐.java
+++ study/1-문자열-누적합-구현/한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class Main {
+public class 한국이 그리울 땐 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

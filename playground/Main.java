7fcff9f1332c42c973e92ce577b7e17b2bf9fade
geniusjun <geniusjun4663@gmail.com>
2026-05-05 19:32:56 +0900

    1-수열 풀이 추가

--- playground/Main.java
+++ playground/Main.java
@@ -1,49 +1,31 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String pattern = br.readLine();
-        int idx = pattern.indexOf("*");
-
-        String start = pattern.substring(0, idx);
-        String last = pattern.substring(idx + 1);
-
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            boolean isOkay = true;
-
-            if (s.length() < start.length() + last.length()) {
-                isOkay = false;
-            }
-
-            if (isOkay) {
-                for (int j = 0; j < start.length(); j++) {
-                    if (s.charAt(j) != start.charAt(j)) {
-                        isOkay = false;
-                        break;
-                    }
-                }
-            }
-
-            if (isOkay) {
-                int cnt = s.length() - 1;
-                for (int j = last.length() - 1; j >= 0; j--) {
-                    if (s.charAt(cnt--) != last.charAt(j)) {
-                        isOkay = false;
-                        break;
-                    }
-                }
-            }
-
-            if (isOkay) {
-                System.out.println("DA");
-            } else {
-                System.out.println("NE");
-            }
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for(int i = 0; i < K; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
         }
+        int mx = sum;
+        for(int i = K; i < N; i++){
+            sum -= arr[i-K];
+            sum += arr[i];
+
+            mx = Math.max(mx,sum);
+
+        }
+
+        System.out.println(mx);
+
     }
 }

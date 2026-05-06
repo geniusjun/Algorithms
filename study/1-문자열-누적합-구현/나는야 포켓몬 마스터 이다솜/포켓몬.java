06f1cd909cbaed16b8c533fcd7c8c699e0cb4a06
geniusjun <geniusjun4663@gmail.com>
2026-05-06 14:28:31 +0900

    1-나는야 포켓몬 마스터 이다솜 풀이 추가

--- study/1-문자열-누적합-구현/나는야 포켓몬 마스터 이다솜/포켓몬.java
+++ study/1-문자열-누적합-구현/나는야 포켓몬 마스터 이다솜/포켓몬.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 포켓몬 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

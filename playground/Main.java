e8a0740864c5493309b0aa4b906cf7910fa99b6d
geniusjun <geniusjun4663@gmail.com>
2026-05-07 16:27:41 +0900

    1-패션왕 신해빈 문제 추가

--- playground/Main.java
+++ playground/Main.java
@@ -7,28 +7,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        Map<String, Integer> nameMap = new HashMap<>();
-        Map<Integer, String> numMap = new HashMap<>();
-
-        for(int i = 1; i <= N; i++){
-            String name = br.readLine();
-            nameMap.put(name, i);
-            numMap.put(i, name);
-        }
-
-        for(int i = 0; i < M; i++){
-            String input = br.readLine();
-            try{
-                int key = Integer.parseInt(input);
-                System.out.println(numMap.get(key));
-            } catch(Exception e){
-                System.out.println(nameMap.get(input));
-            }
-        }
     }
 }

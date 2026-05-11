02a7901fe6003654bf9a6e752e85c823e814cb24
geniusjun <geniusjun4663@gmail.com>
2026-05-11 14:13:56 +0900

    1-좋은 단어 풀이 추가

--- study/1-문자열-누적합-구현/좋은 단어/좋은 단어.java
+++ study/1-문자열-누적합-구현/좋은 단어/좋은 단어.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 좋은 단어 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

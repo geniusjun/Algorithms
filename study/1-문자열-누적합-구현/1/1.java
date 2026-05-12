aba0add2c0fff40c7b8173fa0f19e99596819680
geniusjun <geniusjun4663@gmail.com>
2026-05-12 17:03:23 +0900

    1-1 문제 풀이 추가

--- study/1-문자열-누적합-구현/1/1.java
+++ study/1-문자열-누적합-구현/1/1.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 1 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

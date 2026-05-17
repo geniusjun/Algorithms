cfa14aa9400023b96d10ef1d0a7d1aa584e1d224
geniusjun <geniusjun4663@gmail.com>
2026-05-17 22:45:25 +0900

    2-비밀번호 발음하기 문제 추가

--- playground/Main.java
+++ playground/Main.java
@@ -11,26 +11,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        J = Integer.parseInt(br.readLine());
-        int bucketStart = 0;
-        ret = 0;
-
-        for(int i = 0; i < J; i++){
-            int here = Integer.parseInt(br.readLine()) - 1;
-            if(bucketStart + M - 1 < here){
-                int gap = here - (bucketStart + M - 1);
-                ret += gap;
-                bucketStart += gap;
-            } else if(bucketStart > here){
-                int gap = bucketStart - here;
-                ret += gap;
-                bucketStart -= gap;
-            }
-        }
         System.out.println(ret);
     }
 }
